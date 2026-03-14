4a5c6e0d09da77475dbb16c58886389c7e18268e
Christophe Laudon <ch.laudon@gmail.com>
2026-03-14 12:51:50 +0100

    Initialisation

---
+++ dg-boite-rouge-bleue.mjs
@@ -0,0 +1,7 @@
+Hooks.once("init", () => {
+  console.info("Dangerous Gary Boites rouge et bleue | Initialisation du module...")
+})
+
+Hooks.once("ready", async () => {
+  console.info("Dangerous Gary Boites rouge et bleue | Module prêt")
+})
